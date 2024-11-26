50d3e4061086c1e1f5bfb27a5300093929c0f334
SomosMoa <odoo@somosmoa.net>
2024-11-26 02:48:04 -0600

    Update test_api.py

--- api_integration/controllers/test_api.py
+++ api_integration/controllers/test_api.py
@@ -123,21 +123,21 @@ class TestApi(http.Controller):
                     invoice = order_update.invoice_ids  # Esto devuelve un recordset de facturas relacionadas
                     _logger.info('%s',invoices)
                     _logger.info('%s',invoice)
-                    if invoice:
+                    #if invoice:
                         # Trasladar campos personalizados
-                        invoice.write({
-                            'x_studio_numero': order_update.x_studio_numero,
-                            'x_studio_serie': order_update.x_studio_serie,
-                        })
+                     #   invoice.write({
+                      #      'x_studio_numero': order_update.x_studio_numero,
+                      #      'x_studio_serie': order_update.x_studio_serie,
+                      #  })
 
                         # Validar que las distribuciones analíticas se hayan trasladado correctamente
-                        for invoice_line in invoice.invoice_line_ids:
-                            print(f"Distribución Analítica en línea {invoice_line.id}: {invoice_line.analytic_distribution}")
+                      #  for invoice_line in invoice.invoice_line_ids:
+                      #      print(f"Distribución Analítica en línea {invoice_line.id}: {invoice_line.analytic_distribution}")
 
                         # Publicar la factura (validarla)
-                        invoice.action_post()
+                      #  invoice.action_post()
 
-                        print(f"Factura publicada con ID: {invoice.id}")
+                      #  print(f"Factura publicada con ID: {invoice.id}")
 
 
                     #return self._success_response("Generacion de Pedido de Venta")
