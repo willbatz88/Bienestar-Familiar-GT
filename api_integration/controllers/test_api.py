a4c2846145bd2e7a13705f2b2f185835e34dadd2
SomosMoa <odoo@somosmoa.net>
2025-01-13 11:47:21 -0600

    Update test_api.py

--- api_integration/controllers/test_api.py
+++ api_integration/controllers/test_api.py
@@ -13,6 +13,7 @@ class TestApi(http.Controller):
     def createOrder(self, **kwargs):
         requests = request.httprequest.data.decode()
         data = json.loads(requests)
+        _logger.info('%s',data)
         #client =  request.env['res.partner'].sudo().search([('id', '=', data.get('partner_id'))])
 
         #if not client:
@@ -56,12 +57,13 @@ class TestApi(http.Controller):
             #11	 	V3_UFM
             #12	 	V4_URL
             #13	 	V6_UVG
+            #14     unis
             idVending=data.get("idVending")
-            valor=0
+            valor=10
             if(idVending=="2"):
                 valor=115 
             if(idVending=="4"):
-                valor=13
+                valor=117
             if(idVending=="9"):
                 valor=8
             if(idVending=="10"):
@@ -72,6 +74,8 @@ class TestApi(http.Controller):
                 valor=12
             if(idVending=="13"):
                 valor=11
+            if(idVending=="14"):
+                valor=13
             objetojson={str(valor):100}
             order = request.env['sale.order'].sudo().create({
                 'partner_id': client.id,
@@ -169,5 +173,95 @@ class TestApi(http.Controller):
         except Exception as e:
             return self._error_response("Error al crear el pedido de venta: {}".format(str(e)))
     
-    
-   
+@http.route('/recuperarOrden', auth='public', type='http', csrf=False, methods=['POST'])
+def recuperarOrder(self, **kwargs):
+    requests = request.httprequest.data.decode()
+    data = json.loads(requests)
+    client =  request.env['res.partner'].sudo().search([('id', '=', data.get('nit'))])
+    if not client:
+        client = request.env['res.partner'].sudo().create({
+            'name': data.get('name'),
+            'city': 'Guatemala',
+            'vat': data.get('nit'),
+            'customer_rank': 1,
+            'company_type': 'person',
+            'property_account_receivable_id': 1434,
+            'property_account_payable_id': 783,
+            'company_id': 37,
+            'lang':'es_ES'
+            })
+    try: 
+        order_lines = data.get('order_lines')
+        #2	 	TORRE_TIGO
+        #4	 	Forum Zona Viva
+        #9	 	V1_FONTABELLA
+        #10	 	V2_AVIA
+        #11	 	V3_UFM
+        #12	 	V4_URL
+        #13	 	V6_UVG
+        #14     unis
+        idVending=data.get("idVending")
+        valor=10
+        if(idVending=="2"):
+            valor=115 
+        if(idVending=="4"):
+            valor=117
+        if(idVending=="9"):
+            valor=8
+        if(idVending=="10"):
+            valor=9
+        if(idVending=="11"):
+            valor=25
+        if(idVending=="12"):
+            valor=12
+        if(idVending=="13"):
+            valor=11
+        if(idVending=="14"):
+            valor=13
+        objetojson={str(valor):100}
+        order = request.env['sale.order'].sudo().create({
+            'partner_id': client.id,
+            'partner_invoice_id': client.id,
+            'partner_shipping_id': client.id,
+            'pricelist_id': 1,
+            'company_id': 37,
+            'x_studio_serie': data.get('serie'),
+            'x_studio_numero':data.get('number'),
+            'date_order':data.get('fecha'),
+            'currency_id':'167',
+            'user_id':2,
+        })
+        for line in order_lines:
+            idproducto =  request.env['product.product'].sudo().search([('default_code', '=', line.get('product_id'))])
+            request.env['sale.order.line'].sudo().create({
+                'order_id': order.id,
+                'product_id': idproducto.id,
+                'name':line.get('name'),
+                'product_uom' : 1, 
+                'analytic_distribution':objetojson,
+                'product_uom_qty': line.get('product_uom_qty'),
+            })        
+        order_update = request.env['sale.order'].sudo().browse(order.id)
+        if order_update:
+            try:
+                order_update.action_confirm()
+                order_update.write({
+                        'date_order':data.get('fecha')
+                    })
+                invoices = order_update._create_invoices()
+                invoice = order_update.invoice_ids  # Esto devuelve un recordset de facturas relacionadas
+                _logger.info('%s',invoice)
+                invoices.action_post()
+                response_data = {"finalizado":"hecho"}
+                return Response(
+                    status=200,
+                    content_type="application/json; charset=utf-8",
+                    headers=[("Cache-Control", "no-store"),("Pragma", "no-cache")],
+                    response = json.dumps(response_data)
+                    )
+            except Exception as e :
+                return self._error_response(f"Error al establecer el estado Pedido de Venta {str(e)}")
+        else:
+                return self._error_response("Registro no encontrado.")
+    except Exception as e:
+        return self._error_response("Error al crear el pedido de venta: {}".format(str(e)))
