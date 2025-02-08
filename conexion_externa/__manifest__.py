e85a36c5bd98389bcb5fb20c3c7ea008b61cce70
SomosMoa <odoo@somosmoa.net>
2025-02-08 13:41:52 -0600

    actualizar manifesto

--- conexion_externa/__manifest__.py
+++ conexion_externa/__manifest__.py
@@ -1,10 +1,6 @@
 {
-    "name": "Módulo WebService para Odoo 16",
-    "version": "0.1",
-    "summary": "Envía transferencias de inventario a un WebService",
-    "author": "willim Batz/ Bienestar Familiar",
-    "depends": ["base", "stock"],  # Se añade 'mail' para seguimiento
-    "data": [
-     #   "security/ir.model.access.csv",  # Si necesitas definir permisos
-    ]
+    "name": "conexion Webservice",
+    "version": "1.0",
+    "depends": ["base", "stock"],
+    "installable": True,
 }
