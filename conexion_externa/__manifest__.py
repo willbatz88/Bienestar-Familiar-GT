b643f12f05f3ca037eeae4b5f6c7d550b85e8b3b
SomosMoa <odoo@somosmoa.net>
2025-02-08 13:59:52 -0600

    actualizar nombre modulo

--- conexion_externa/__manifest__.py
+++ conexion_externa/__manifest__.py
@@ -1,6 +1,9 @@
 {
     "name": "conexion Webservice",
+    'author': "Somos Moa",
     "version": "1.0",
+    'category': 'Ventas',
+    'version': '0.1',
     "depends": ["base", "stock"],
     "installable": True,
 }
