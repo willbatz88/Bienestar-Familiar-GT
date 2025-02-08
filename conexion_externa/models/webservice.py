5d626cea302617fbc9a32f0b3f72fb59311a057a
SomosMoa <odoo@somosmoa.net>
2025-02-08 13:47:55 -0600

    cambiar nomeclatura

--- conexion_externa/models/webservice.py
+++ conexion_externa/models/webservice.py
@@ -1,21 +1,21 @@
 import requests
 import json
-import logging
-from odoo import models, api, fields, _
-from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
-
+from odoo import models, fields, api
 class WebServiceConnector(models.Model):
     _name = "web.service.connector"
     _description = "Conector para enviar transferencias a un WebService"
-    
-    
     response_message = fields.Text(string="Respuesta del Servidor")
-
     @api.model
     def enviar_transferencia(self, picking_id):
         try:
+            url = "http://busservicio-dev.eba-epxjazui.us-east-1.elasticbeanstalk.com/api/IntegrateSrFrm/pruebaConexion?cadena=2&nombre=desdeendpoint"
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise Exception(f"Error en la solicitud: {response.status_code} - {response.text}")
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            raise UserError(_("Error al enviar la transferencia: %s") % str(e))
+
         #picking = self.env["stock.picking"].browse(picking_id)
         
         # Datos de la transferencia
@@ -38,11 +38,3 @@ class WebServiceConnector(models.Model):
         #}
         # Enviar solicitud
 #        response = requests.post(url, json=datos_transferencia, headers=headers)
-            url = "http://busservicio-dev.eba-epxjazui.us-east-1.elasticbeanstalk.com/api/IntegrateSrFrm/pruebaConexion?cadena=2&nombre=desdeendpoint"
-            response = requests.get(url)
-            if response.status_code != 200:
-                raise Exception(f"Error en la solicitud: {response.status_code} - {response.text}")
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            raise UserError(_("Error al enviar la transferencia: %s") % str(e))
-
