2b582158b530aadd3604be3f94e536d446d0f8ca
SomosMoa <odoo@somosmoa.net>
2025-02-08 18:23:13 -0600

    Update stock_picking_extension.py

--- albaran_extension/models/stock_picking_extension.py
+++ albaran_extension/models/stock_picking_extension.py
@@ -10,23 +10,5 @@ class StockPicking(models.Model):
     referencia_externa = fields.Char(string="Referencia Externa", help="Campo para almacenar una referencia externa")
 
     def confirmar_y_notificar(self):
-        """
-        Método personalizado que cambia el estado a 'done' y notifica al usuario
-        """
-        for picking in self:
-            if picking.state not in ['confirmed', 'assigned']:
-                raise UserError("El albarán debe estar en estado 'Confirmado' o 'Reservado' para completarlo.")
-
-            # Cambiar el estado del albarán a 'hecho'
-            picking.button_validate()
-
-            # Registrar en el log
-            _logger.info(f"Albarán {picking.name} confirmado y notificado.")
-
-            # Enviar una notificación al usuario
-            picking.message_post(
-                body=f"El albarán <b>{picking.name}</b> ha sido confirmado y completado.",
-                message_type="notification"
-            )
-
+        _logger.info(f"Albarán {picking.name} confirmado y notificado.")
         return True
