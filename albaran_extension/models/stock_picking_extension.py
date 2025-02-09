82f45f9e9c66988c715a559bfedc270420d4949b
SomosMoa <odoo@somosmoa.net>
2025-02-08 18:41:41 -0600

    Update stock_picking_extension.py

--- albaran_extension/models/stock_picking_extension.py
+++ albaran_extension/models/stock_picking_extension.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"  # Extendemos el modelo de albaranes
-        
-    def confirmar_y_notificar(self):
+
+    def _confirmar_y_notificar(self):
         _logger.info(f"Albarán confirmado y notificado.")
         return True
