9a2d941942a3c66991a5714654d9ec823f1ab7b3
SomosMoa <odoo@somosmoa.net>
2025-02-11 20:57:51 -0600

    Update sale_order.py

--- venta_extension/models/sale_order.py
+++ venta_extension/models/sale_order.py
@@ -11,13 +11,15 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"  # Extendemos el modelo de albaranes
     def obtener_fecha_formateada():
     # Obtener la fecha actual
-        fecha_actual = datetime.now()
+        
     # Convertir la fecha al formato deseado: 'yyyy-mm-dd'
-        fecha_formateada = fecha_actual.strftime("%Y-%m-%d")
+        
         return fecha_formateada
 
     def action_mi_accion(self):
         _logger.info(f"Venta confirmado y notificado."+str(self.id))
+        fecha_actual = datetime.now()
+        fecha_formateada = fecha_actual.strftime("%Y-%m-%d")
         # Definir la URL del servicio web
         url = "https://srfarmacia.bf-transac.com/api/IntegrateSrFrm/NotificarVenta"
         datos_transferencia = {
@@ -27,7 +29,7 @@ class SaleOrder(models.Model):
             "provider_id": 1,
             "payment_method_id": 1,
             "origin":"Bodega Central",
-            "scheduled_date":obtener_fecha_formateada(),
+            "scheduled_date":fecha_formateada,
             "id_sistema_origen":self.name,
             "invoice":"32323",
             "serie":"234234234",
