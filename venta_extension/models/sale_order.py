4d69fafc3436a8bac6a67e066f85d0433dcfd6b6
SomosMoa <odoo@somosmoa.net>
2025-02-11 21:29:23 -0600

    Update sale_order.py

--- venta_extension/models/sale_order.py
+++ venta_extension/models/sale_order.py
@@ -29,10 +29,11 @@ class SaleOrder(models.Model):
             producto= {
                     "product_id": base,
                     "name": product.name,
-                    "product_uom_qty":str(line.product_uom_qty),
+                    "product_uom_qty":line.product_uom_qty,
                     "product_uom":1,
                     "unit_cost":product.standard_price
-                } 
+                }
+            _logger.info(producto) 
             base=base+1
             lista.append(producto)
 
