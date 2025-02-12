f81f8ec25a4de44a57b45b13bc45825b744fbc33
SomosMoa <odoo@somosmoa.net>
2025-02-11 20:23:01 -0600

    creacion de modulo para ventas
    agregar extension vista

---
+++ venta_extension/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Extensión de Ventas en Odoo 16",
+    "version": "16.0.1.0.0",
+    "summary": "Añade métodos personalizados a las Ventas (order_sales)",
+    "author": "Tu Nombre / Bienestar Familiar",
+    "depends": ["base", "sale"],
+    "data": ["views/sale_order_view.xml"],
+    "installable": True,
+    "application": False,
+}
---
+++ venta_extension/models/sale_order.py
@@ -0,0 +1,45 @@
+import logging
+import requests
+import json 
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"  # Extendemos el modelo de albaranes
+
+    def confirmar_y_notificar(self):
+        _logger.info(f"Venta confirmado y notificado."+str(self.id))
+        # Definir la URL del servicio web
+        url = "https://srfarmacia.bf-transac.com/api/IntegrateSrFrm/NotificarVenta"
+        datos_transferencia = {
+            "picking_type_id": 1,
+            "location_origin_id": 1,
+            "location_dest_id": 1,
+            "provider_id": 1,
+            "payment_method_id": 1,
+            "origin":"Bodega Central",
+            "scheduled_date":"2025-02-28",
+            "id_sistema_origen":"Odoo-003",
+            "invoice":"0",
+            "serie":"0",
+            "moves": [
+                {
+                    "product_id": 240,
+                    "name": "[P017896] TYLENOL EXTRAFUERTE 500MG 20 CAPSULA (10 SOBRE X 2)",
+                    "product_uom_qty":25,
+                    "product_uom":1,
+                    "unit_cost":10.00
+                } 
+            ]
+        }
+        headers = {"Content-Type": "application/json"}
+        #response=requests.get(url)
+        response = requests.post(url, data=json.dumps(datos_transferencia),headers=headers)
+        # Verificar la respuesta
+        _logger.info("codigo: "+str(response.status_code))
+        if response.status_code != 200:
+            raise Exception(f"Error en la solicitud: {response.status_code} - {response.text}")
+        
+        return True
